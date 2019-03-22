030bfcc96c0cc52675a19abe938397dc990d711c
Ely Galdino dos Santos Junior <ext.ely.junior@DFT-SP-WKN571.dafiti.local>
2019-03-22 15:55:43 -0300

    add inGameState

---
+++ src/components/inGameState.js
@@ -0,0 +1,29 @@
+export default{
+    location:{
+        x: 2019,
+        y: 1461,
+        z: 1584
+    },
+    phase: 'freefly',
+    match_info: {
+        headshots: "0",
+        kills: "1",
+        map: "Savege_Main",
+        max_kill_distance: "4610.637207",
+        me: "14",
+        mode: "solo",
+        nicknames: {team_members:{}},
+        rosters:[{
+            player:'Conkers_Mrc',
+            kills: '0',
+            out:'false'
+        }],
+        total: "98",
+        total_damage_dealt: "184.319",
+        total_teams: "98"
+    },
+    me: {
+        name:"Conkers_Mrc",
+        status: "success"
+    }
+}
